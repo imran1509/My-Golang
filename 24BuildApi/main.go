4083c2e16afb535756f1f7958c0c2666705d4c8e
Mohd Imran <iimran2508@gmail.com>
2023-10-22 18:25:11 +0530

    addedcourse controllers in Build API

--- 24BuildApi/main.go
+++ 24BuildApi/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
 //model for course - separate file
@@ -26,7 +31,8 @@ var courses []Course
 //middleware, helper - separate file
 
 func (c *Course) IsEmpty() bool {
-	return c.CourseID == "" && c.CourseName == ""
+	//return c.CourseID == "" && c.CourseName == ""
+	return c.CourseName == ""
 }
 
 func main() {
@@ -35,12 +41,56 @@ func main() {
 
 //controllers - separate file
 
-func serveHome(w http.ResponseWriter, r http.Request) {
+func serveHome(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to API by Imran</h1>"))
 }
 
-func getAllcourses(w http.ResponseWriter, r http.Request) {
+func getAllcourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("content-type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("content-type", "application/json")
+
+	//grab id from request
+	params := mux.Vars(r)w.Header().Set("content-type", "application/json")
+
+	//loop through courses, find matching id and return the response
+	for _, course := range courses {
+		if course.CourseID == params["ID"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode("No response found with given ID")
+	return
+}
+
+func createOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Create one course")
+	w.Header().Set("content-type", "application/json")
+
+	//What if: body is empty
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Please send some data")
+	}
+
+	//what about - {}
+	var course Course
+	_ = json.NewEncoder(r.Body).Decode(&course)
+	if course.IsEmpty() {
+		json.NewEncoder(w).Encode("No data inside JSON")
+		return 
+	}
+	//generate unique id, convert it into strings
+	//append course into courses
+
+	rand.Seed(time.Now().UnixNano())
+	course.CourseID = strconv.Itoa(rand.Intn(100))
+	courses = append(courses, course)
+	json.NewEncoder(w).Encode(course)
+	return
 }
