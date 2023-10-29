436229faef43f7fe4e6816944c45c74b8f880e61
Mohd Imran <iimran2508@gmail.com>
2023-10-29 16:52:23 +0530

    added code for adding 1 record and updating 1 record to database in controller file

--- 25MongoApi/controller/controller.go
+++ 25MongoApi/controller/controller.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/imran1509/MongoApi/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/x/mongo/driver/mongocrypt/options"
@@ -36,4 +39,29 @@ func init() {
 
 	//collecton instance
 	fmt.Println("colection instance is ready")
+}
+
+//MONGODB helpers
+
+// insert 1 record
+func insertOne(movie model.Netflix) {
+	inserted, err := collection.InsertOne(context.Background(), movie)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("inserted 1 movie in db with id: ", inserted.InsertedID)
+}
+
+// update 1 record
+func updateOnemovie(movieID string) {
+	id, _ := primitive.ObjectIDFromHex(movieID)
+	filter := bson.M{"_id": id}
+	update := bson.M{"$set": bson.M{"watched": true}}
+
+	result, err := collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("modified count: ", result.ModifiedCount)
 }
